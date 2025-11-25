4d8208fd6f27bdc78eee4e692ab8d8f81971f468
Ardelia <ardeliautomo@gmail.com>
2025-11-25 12:17:03 +0800

    first commit

---
+++ HarryPotterWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HarryPotterWiki
+//
+//  Created by Ardelia on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HarryPotterWiki/HarryPotterWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterWikiApp.swift
+//  HarryPotterWiki
+//
+//  Created by Ardelia on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
